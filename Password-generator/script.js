4262130dae53818abc982727aa0d6a4a377248e5
KANEEZ-FATIMA18 <k.fatima182005@gmail.com>
2025-09-02 23:36:31 +0500

    Add Password Generator application with HTML, CSS, and JavaScript files

---
+++ Password-generator/script.js
@@ -0,0 +1,71 @@
+
+
+
+// Password generation logic
+var copy = document.getElementById("copyBtn");
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*";
+var allChars = upperCase + lowerCase + numbers + symbols;
+
+
+var generate = document.getElementById("Password");
+var regenerate = document.getElementsByClassName("Regenerate-Password");
+
+//length
+var lenVal = document.getElementById("lengthValue");
+var len = document.getElementById("length");
+
+
+len.oninput = function () {
+  lenVal.textContent = this.value;
+}
+
+function generatePassword() {
+  var allChars = "";
+  if (document.getElementById("includeUppercase").checked) {
+    allChars += upperCase;
+  }
+  if (document.getElementById("includeLowercase").checked) {
+    allChars += lowerCase;
+  }
+  if (document.getElementById("includeNumbers").checked) {
+    allChars += numbers;
+  }
+  if (document.getElementById("includeSymbols").checked) {
+    allChars += symbols;
+  }
+
+  password = "";
+  for (var i = 0; i < len.value; i++) {
+    var randomNumber = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomNumber]
+    generate.innerHTML = password;
+  }
+
+
+  return password;
+
+}
+
+
+function copyPassword() {
+  // password box ko pakadna
+  var passwordBox = document.getElementById("Password");
+
+  // password ki value lena
+  var password = passwordBox.textContent;
+
+  navigator.clipboard.writeText(password);
+
+  if (password) {
+    // message dikhana
+    alert("Password copied!");
+  }
+  else {
+    alert("Failed Password")
+  }
+
+}
