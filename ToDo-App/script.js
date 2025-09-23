8823bb65285e2048d4ed086299a02533446d6f38
KANEEZ-FATIMA18 <k.fatima182005@gmail.com>
2025-09-23 18:19:04 +0500

    Enhance ToDo application: add edit functionality and style updates for icons and input

--- ToDo-App/script.js
+++ ToDo-App/script.js
@@ -3,55 +3,75 @@ var addButton = document.querySelector("#add-btn");
 var Ul = document.querySelector("#ul")
 
 function AddTask() {
-    // if taskinput is empty 
     if (TaskInput.value == "") {
         TaskInput.style.border = '2px solid red';
-
-
     } else {
-        //create li for task list
-        var li = document.createElement("li")
-        li.className = 'task-item'
-        //create delete icon
-        var deleteIcon = document.createElement('i')
-        deleteIcon.className = 'fa-solid fa-trash icon'
+        // create li
+        var li = document.createElement("li");
+        li.className = 'task-item';
 
-        //create checkbox with classname & type
-        var checkbox = document.createElement('input')
-        checkbox.type = 'checkbox'
-        checkbox.className = 'checkbox'
+        // create checkbox
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.className = 'checkbox';
 
-        // Capitalize first letter
-        li.textContent = TaskInput.value.charAt(0).toUpperCase() + TaskInput.value.slice(1);
-        //add icon in list
-        li.prepend(checkbox)
+        // create span for text
+        var span = document.createElement("span");
+        span.textContent = TaskInput.value.charAt(0).toUpperCase() + TaskInput.value.slice(1);
 
+        // create delete icon
+        var deleteIcon = document.createElement('i');
+        deleteIcon.className = 'fa-solid fa-trash icon';
 
-        Ul.appendChild(li)         //add li in ul
+        // create edit icon
+        var editIcon = document.createElement('i');
+        editIcon.className = 'fa-solid fa-pen-to-square icon';
 
-        li.append(deleteIcon)    //add icon in list
-        //input reset 
-        TaskInput.value = ""
+        // append all
+        li.appendChild(checkbox);
+        li.appendChild(span);
+        li.appendChild(editIcon);
+        li.appendChild(deleteIcon);
+        Ul.appendChild(li);
+
+        // reset input
+        TaskInput.value = "";
         TaskInput.style.border = '';
 
         // checkbox logic
         checkbox.addEventListener("change", function () {
             if (checkbox.checked) {
-                li.style.textDecoration = 'line-through'
-                li.style.color ='green'
-            } else { li.style.textDecoration = "none";
-                li.style.color =''
-             }
-        })
-        //delete icon logic
+                span.style.textDecoration = 'line-through';
+                span.style.color = 'green';
+            } else {
+                span.style.textDecoration = "none";
+                span.style.color = '';
+            }
+        });
+
+        // delete logic
         deleteIcon.addEventListener("click", function () {
-            li.remove()
-        })
+            li.remove();
+        });
+        // edit logic
+        editIcon.addEventListener("click", function () {
+            var editInput = document.createElement("input");
+            editInput.type = "text";
+            editInput.className = "edit-input";
+            editInput.value = span.textContent;
+          
+            li.replaceChild(editInput, span);
 
+            
+            editInput.addEventListener("keypress", function (e) {
+                if (e.key === "Enter") {
+                    span.textContent = editInput.value;
+                    li.replaceChild(span, editInput);
+                }
+            });
 
+            editInput.focus();
+        });
     }
-
 }
-
-
 
